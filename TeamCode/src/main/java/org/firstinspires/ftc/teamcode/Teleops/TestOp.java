f548275ba073d4ccc7c6d1ca57084f781dde1849
Aaron M <vaz.nightwalker@gmail.com>
2025-08-28 13:21:21 +0200

    gg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.Teleops;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -15,6 +14,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @TeleOp(name="Dashboard Speed Control", group="Testing")
 
 //@TeleOp       //if this is a teleop
+//@Autonomous   //if this is an auto
 public class TestOp extends OpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
