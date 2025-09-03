ace26c8e955cc9aaf0a12fca10f7c857aab2b6ca
Aaron M <vaz.nightwalker@gmail.com>
2025-09-03 14:18:50 +0200

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/BlankOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/BlankOp.java
@@ -1,70 +1,94 @@
 package org.firstinspires.ftc.teamcode.Teleops;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-//@Config       //if you want config
-//@TeleOp       //if this is a teleop
-@Autonomous   //if this is an auto
+@Config
+@TeleOp(name="MyMotorWithEncoder")
 public class BlankOp extends OpMode {
-    // Declare OpMode members.
+    // Timer
     private ElapsedTime runtime = new ElapsedTime();
+
+    // Configurable motor power
+    public static double DRIVE_POWER = 0.5;
+
+    // Encoder target positions (you can tune in FTC Dashboard)
+    public static int targetUpPosition = 1000;
+    public static int targetDownPosition = 0;
+
+    // Gamepad helper
     GamepadEx g1;
 
-    //this section allows us to access telemetry data from a browser
+    // FTC Dashboard
     FtcDashboard dashboard = FtcDashboard.getInstance();
     Telemetry dashboardTelemetry = dashboard.getTelemetry();
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+    // Motor
+    DcMotor myMotor;
+
     @Override
     public void init() {
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step
         g1 = new GamepadEx(gamepad1);
 
-        // Tell the driver that initialization is complete.
+        // Initialize motor
+        myMotor = hardwareMap.get(DcMotor.class,"myMotor");
+
+        // Reset encoder and set to run-to-position
+        myMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        myMotor.setTargetPosition(targetDownPosition);
+        myMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Telemetry
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
         dashboardTelemetry.addData("Status", "Initialized");
         dashboardTelemetry.update();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits START
-     */
     @Override
     public void start() {
         runtime.reset();
+        myMotor.setPower(DRIVE_POWER);
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
-     */
     @Override
     public void loop() {
         g1.readButtons();
+
+        // Button controls for encoder positions
+        if (gamepad1.a) {
+            myMotor.setTargetPosition(targetUpPosition);
+            myMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            myMotor.setPower(DRIVE_POWER);
+        }
+        if (gamepad1.b) {
+            myMotor.setTargetPosition(targetDownPosition);
+            myMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            myMotor.setPower(DRIVE_POWER);
+        }
+
+        // Telemetry
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Target Position", myMotor.getTargetPosition());
+        telemetry.addData("Current Position", myMotor.getCurrentPosition());
+        telemetry.addData("Drive Power", DRIVE_POWER);
+        telemetry.update();
+
         dashboardTelemetry.addData("Status", "Run Time: " + runtime.toString());
+        dashboardTelemetry.addData("Target Position", myMotor.getTargetPosition());
+        dashboardTelemetry.addData("Current Position", myMotor.getCurrentPosition());
+        dashboardTelemetry.addData("Drive Power", DRIVE_POWER);
         dashboardTelemetry.update();
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
     @Override
     public void stop() {
+        myMotor.setPower(0);
     }
 }
