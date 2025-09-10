3225ca5e31422c443d9927a132fc65e52b8b3105
Aaron M <vaz.nightwalker@gmail.com>
2025-09-10 14:04:24 +0200

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
@@ -1,18 +1,23 @@
 package org.firstinspires.ftc.teamcode.Teleops;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 //import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @Config       //if you want config
-@TeleOp
+@TeleOp (name = "Aarons code")
 
 //@TeleOp       //if this is a teleop
 //@Autonomous   //if this is an auto
@@ -20,7 +25,8 @@ public class TestOp extends OpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     public static double DRIVE_POWER = 0.5;  // default speed (0.0 - 1.0)
-
+    public static int positon = 10000;
+    IMU imu;
 
     GamepadEx g1;
     FtcDashboard dashboard = FtcDashboard.getInstance();
@@ -38,9 +44,15 @@ public class TestOp extends OpMode {
         g1 = new GamepadEx(gamepad1);
 
         myMotor = hardwareMap.get(DcMotor.class,"myMotor");
+        imu = hardwareMap.get(IMU.class,"imu");
         myMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
         myMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Turn the motor back on when we are done
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
         // Tell the driver that initialization is complete.
         dashboardTelemetry.addData("Status", "Initialized");
         dashboardTelemetry.update();
@@ -68,13 +80,22 @@ public class TestOp extends OpMode {
     @Override
     public void loop() {
         g1.readButtons();
-
-        // ✅ Use DRIVE_POWER to control the motor
-        myMotor.setPower(DRIVE_POWER);
-        // Send info to Dashboard telemetry
+        dashboardTelemetry.addData("yaw",imu.getRobotYawPitchRollAngles().getYaw());
+        dashboardTelemetry.addData("pitch",imu.getRobotYawPitchRollAngles().getPitch());
+        dashboardTelemetry.addData("roll",imu.getRobotYawPitchRollAngles().getRoll());
+        myMotor.setPower(-imu.getRobotYawPitchRollAngles().getYaw());
+        if( imu.getRobotYawPitchRollAngles().getYaw()> 0){
+            myMotor.setPower(-DRIVE_POWER);
+        }else{
+            myMotor.setPower(DRIVE_POWER);
+        }
         dashboardTelemetry.addData("Drive Power", DRIVE_POWER);
+        dashboardTelemetry.update();
         dashboardTelemetry.addData("motor ticks", myMotor.getCurrentPosition());
         dashboardTelemetry.addData("Status", "Run Time: " + runtime.toString());
+        myMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        myMotor.setTargetPosition(positon);
+        dashboardTelemetry.addData("position",positon);
         dashboardTelemetry.update();
     }
 
@@ -84,5 +105,6 @@ public class TestOp extends OpMode {
     @Override
     public void stop() {
         myMotor.setPower(0);
+
     }
 }
