2cebfea1d7fff2fa2f667cacb1a1671b32a52562
Aaron M <vaz.nightwalker@gmail.com>
2025-09-19 14:41:07 +0200

    This is trial #36

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
@@ -17,29 +17,30 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @Config
-@TeleOp (name = "Trial 35")
+@TeleOp (name = "Trial 36")
 //@Autonomous
 public class TestOp extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
     public static double DRIVE_POWER = 0;
+    public static double ARM_POWER = 0;
     public static int positon = 10000;
     IMU imu;
     GamepadEx g1;
     FtcDashboard dashboard = FtcDashboard.getInstance();
     Telemetry dashboardTelemetry = dashboard.getTelemetry();
-    DcMotor myMotor;
+    DcMotor armMotor;
     DcMotor myMotor2;
     DcMotor myMotor3;
 
     @Override
     public void init() {
         g1 = new GamepadEx(gamepad1);
-        myMotor = hardwareMap.get(DcMotor.class,"myMotor");
+        armMotor = hardwareMap.get(DcMotor.class,"armMotor");
         myMotor2 = hardwareMap.get(DcMotor.class,"myMotor2");
         myMotor3 = hardwareMap.get(DcMotor.class,"myMotor3");
         imu = hardwareMap.get(IMU.class,"imu");
-        myMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        myMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         myMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         myMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         myMotor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -73,7 +74,7 @@ public class TestOp extends OpMode {
         double turn  = gamepad1.right_stick_x;
         double leftPower  = Range.clip(drive + turn, -1.0, 1.0);
         double rightPower = Range.clip(drive - turn, -1.0, 1.0);
-
+        armMotor.setPower(ARM_POWER);
         myMotor3.setPower(leftPower);
         myMotor2.setPower(rightPower);
         if (Math.abs(turn) > 0.05) {
@@ -86,49 +87,43 @@ public class TestOp extends OpMode {
         dashboardTelemetry.addData("roll", imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.DEGREES));
 
         if( imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)< -2){
-            myMotor.setPower(-imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
             myMotor2.setPower(-imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)/10);
             myMotor3.setPower(imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)/10);
             if (imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES) < -5){
                 stop();
-                myMotor.setPower(0);
                 myMotor2.setPower(0);
                 myMotor3.setPower(0);
             }
         } else if (imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES) > 2) {
-            myMotor.setPower(imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
             myMotor2.setPower(-imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)/10);
             myMotor3.setPower(imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)/10);
             if (imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES) > 5){
                 stop();
-                myMotor.setPower(0);
                 myMotor2.setPower(0);
                 myMotor3.setPower(0);
             }
         } else {
-            myMotor.setPower(0);
             myMotor2.setPower(DRIVE_POWER);
             myMotor3.setPower(DRIVE_POWER);
         }
 
         dashboardTelemetry.addData("Drive Power", DRIVE_POWER);
-        dashboardTelemetry.addData("motor ticks", myMotor.getCurrentPosition());
+        dashboardTelemetry.addData("Arm Power",ARM_POWER);
+        dashboardTelemetry.addData("motor ticks", armMotor.getCurrentPosition());
         dashboardTelemetry.addData("motor 2 ticks",myMotor2.getCurrentPosition());
         dashboardTelemetry.addData("motor 3 ticks",myMotor3.getCurrentPosition());
         dashboardTelemetry.addData("Status", "Run Time: " + runtime.toString());
-        myMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        myMotor.setTargetPosition(positon);
         dashboardTelemetry.addData("position",positon);
         dashboardTelemetry.update();
     }
 
     @Override
     public void stop() {
-        myMotor.setPower(0);
+        armMotor.setPower(0);
         myMotor2.setPower(0);
         myMotor3.setPower(0);
-        myMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        myMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         myMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         myMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         myMotor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
