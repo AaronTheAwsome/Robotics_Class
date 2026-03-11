21e26ad04a99df61ef50dbffaccf4e4877352359
amcl7032 <vaz.nightwalker@gmail.com>
2026-03-11 13:48:15 +0100

    k

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
@@ -1,78 +1,180 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
+import org.firstinspires.ftc.teamcode.Subsystems.GoBildaPinpointDriver;
+
+import javax.annotation.Nonnull;
+
+// referenced https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+
+@Config
 public class DriveSubsystem {
+    public static double correctionMultiplier = 1;
+    public static double ANGULAR_TOLERANCE_DEGREES = 2;
+    public static double rotMulti = 1.1;
+    private final DcMotorEx leftFront, leftBack, rightBack, rightFront;
+    public GoBildaPinpointDriver pinpoint;
+    private double headingToMaintain = 0;
+    //limits how fast human can rotate robot, decrease to slow down rotation
+    public double rotation_multi = 0.5;
+    //acceptable angle tolerance in radians
+    public double ANGULAR_TOLERANCE = Math.PI/90;
 
-    private final DcMotor leftMotor;   // myMotor3
-    private final DcMotor rightMotor;  // myMotor2
-    private final IMU imu;
+    public DriveSubsystem(@Nonnull HardwareMap hardwareMap){
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightRear");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
 
-    public DriveSubsystem(HardwareMap hardwareMap) {
-        leftMotor  = hardwareMap.get(DcMotor.class, "myMotor3");
-        rightMotor = hardwareMap.get(DcMotor.class, "myMotor2");
-        imu        = hardwareMap.get(IMU.class, "imu");
 
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        leftMotor.setDirection(DcMotor.Direction.FORWARD);
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
-        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP;
-        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
-        imu.initialize(new IMU.Parameters(orientationOnRobot));
-        imu.resetYaw();
-    }
-
-    public void driveTank(double drive, double turn) {
-        double leftPower  = Range.clip(drive + turn, -1.0, 1.0);
-        double rightPower = Range.clip(drive - turn, -1.0, 1.0);
-
-        leftMotor.setPower(leftPower);
-        rightMotor.setPower(rightPower);
-    }
-
-    public void headingHold(double turnDeadband, double kP) {
-        double turnInput = 0; // you pass in gamepad turn, see OpMode below
-
-        // If you want the subsystem to fully handle it, give headingHold the turn:
-        // and save it in a field instead, but to keep this simple we will call this
-        // only when |turn| < deadband in the OpMode.
-
-        double yaw = imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.DEGREES);
-
-        if (Math.abs(yaw) > 2) {
-            double correction = -yaw * kP;
-            correction = Range.clip(correction, -0.3, 0.3);
-            leftMotor.setPower(correction);
-            rightMotor.setPower(-correction);
+        // TODO: reverse motor directions if needed
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        pinpoint.resetPosAndIMU();
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public double getYaw()   { return imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.DEGREES); }
-    public double getPitch() { return imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.DEGREES); }
-    public double getRoll()  { return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES); }
+    //***************************************************
+    //pulled from 2024 drive code to integrate Pinpoint
+    //***************************************************
 
-    public int getLeftTicks()  { return leftMotor.getCurrentPosition(); }
-    public int getRightTicks() { return rightMotor.getCurrentPosition(); }
-
-    public void resetHeading() {
-        imu.resetYaw();
+    //returns the current heading of the robot in RAD
+    public double calcYaw() {
+        pinpoint.update(GoBildaPinpointDriver.readData.ONLY_UPDATE_HEADING);
+        return Math.round(pinpoint.getHeading()*10)/10.0;
     }
 
-    public void stop() {
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
+    //determines the shortest path to desired angle in degrees
+    public double figureOutWhatIsShorter(double reading) {
+        double result;
+        double oppositeButEqualReading;
+
+        if (reading > 0) {
+            oppositeButEqualReading = reading - 360;
+        } else {
+            oppositeButEqualReading = reading + 360;
+        }
+
+        double normalReadingDifference = Math.abs(this.headingToMaintain - reading);
+        double oppositeReadingDifference = Math.abs(this.headingToMaintain - oppositeButEqualReading);
+        boolean isOppositeReadingShorter =
+                normalReadingDifference > oppositeReadingDifference;
+
+        if (isOppositeReadingShorter) {
+            result = this.headingToMaintain - oppositeButEqualReading;
+        } else {
+            result = this.headingToMaintain - reading;
+        }
+        return -result;
     }
+
+    //return a value that is below or equal to the limit
+    private double limiter(double input, double lim) {
+        //this will limit the pid to a range of -1 to 1
+        if (input > lim) {
+            input = lim;
+        } else if (input < -lim) {
+            input = -lim;
+        }
+        return input;
+    }
+
+    public void setHeadingToMaintain(double input){
+        this.headingToMaintain = input;
+    }
+
+
+    public void drive2(double x, double y, double rx){
+        x = -x;
+        y = -y;
+        double robotHeadingRAD = calcYaw();
+        double robotHeadingDEG = Math.toDegrees(robotHeadingRAD);
+
+        if(rx == 0){
+            //we're trying to maintain our current heading
+            //calc the shortest deviation to target heading in degrees
+            double shorter = this.figureOutWhatIsShorter(robotHeadingDEG);
+            //check if we are within tolerance
+            boolean isWithinAngularTolerance =
+                    Math.abs(shorter) < ANGULAR_TOLERANCE_DEGREES;
+
+            //we turn if we're not within a tolerance
+            if(!isWithinAngularTolerance){
+                //this means we are moving
+                if (Math.abs(y) > 0 || Math.abs(x) > 0) {
+                    double rotSpeed = Math.abs(shorter);
+                    if (rotSpeed > 20) {
+                        rotSpeed = 1;
+                    } else {
+                        rotSpeed = correctionMultiplier * rotSpeed * rotSpeed / 800.0;
+                    }
+                    rx = limiter(shorter, rotSpeed);
+                } else {
+                    //this means we are not moving but not pointing in the right direction
+                    double rotSpeed = Math.abs(shorter);
+                    if (rotSpeed > 20) {
+                        rotSpeed = 1;
+                    } else {
+                        rotSpeed = 2 * correctionMultiplier * rotSpeed * rotSpeed / 800.0;
+                    }
+                    rx = limiter(shorter, rotSpeed);
+                }
+            }
+        }else{
+            //we're going to maintain our new heading once we've stopped turning.
+            //not before we've turned
+            this.headingToMaintain = robotHeadingDEG;
+        }
+        //triangle """magic"""
+        double rotX = x * Math.cos(-robotHeadingRAD) - y * Math.sin(-robotHeadingRAD);
+        double rotY = x * Math.sin(-robotHeadingRAD) + y * Math.cos(-robotHeadingRAD);
+        rotX = rotX * rotMulti;
+
+        //double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        //double frontLeftPower = (y+x+rx) / denominator;
+        //double backLeftPower = (y - x + rx) / denominator;
+        //double frontRightPower = (y-x-rx) / denominator;
+        //double backRightPower = (y + x - rx) / denominator;
+
+        double denominator = Math.max(Math.abs(rotX) + Math.abs(rotY) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx)  / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx)   / denominator;
+        double backRightPower = (rotY + rotX - rx)  / denominator;
+
+        leftBack.setPower(backLeftPower);
+        rightBack.setPower(backRightPower);
+        leftFront.setPower(frontLeftPower);
+        rightFront.setPower(frontRightPower);
+    }
+
+    private double map(double x, double in_min, double in_max, double out_min, double out_max) {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    public double getHeadingToMaintain(){
+        return headingToMaintain;
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/TestOp.java
@@ -1,24 +1,19 @@
 package org.firstinspires.ftc.teamcode.Teleops;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.GoBildaPinpointDriver;
+import java.sql.Driver;
 
 @Config
 @TeleOp(name = "BattleDroid Manual Control")
@@ -51,6 +46,7 @@ public class TestOp extends OpMode {
 
         runtime.reset();
 
+
         dashboardTelemetry.addData("Status", "Initialized");
         dashboardTelemetry.addData("launchSpeed","launchMotor");
         dashboardTelemetry.update();
@@ -65,19 +61,12 @@ public class TestOp extends OpMode {
     public void loop() {
         g1.readButtons();
 
-        double driveInput = -gamepad1.left_stick_y;  // usually invert for forward
-        double turnInput  = gamepad1.right_stick_x;
-
-        // basic driving
-        drive.driveTank(driveInput, turnInput);
+        double driveInput = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double turnInput = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+        drive.drive2( driveInput, turnInput, rx);
 
         // heading hold (your previous logic)
-        if (Math.abs(turnInput) < 0.07) {
-            // simple P controller: adjust kP to match your old /50.0 behavior
-            drive.headingHold(0.07, 1.0 / 50.0);
-        } else {
-            drive.resetHeading();
-        }
 
         // intake / lift
         if (gamepad1.x) {
@@ -108,26 +97,17 @@ public class TestOp extends OpMode {
        // }
 
         // Telemetry
-        double yaw   = drive.getYaw();
-        double pitch = drive.getPitch();
-        double roll  = drive.getRoll();
 
-        dashboardTelemetry.addData("yaw", yaw);
-        dashboardTelemetry.addData("pitch", pitch);
-        dashboardTelemetry.addData("roll", roll);
         dashboardTelemetry.addData("position", position);
         dashboardTelemetry.addData("Launch Power", LAUNCH_POWER);
         dashboardTelemetry.addData("Drive Power", DRIVE_POWER);
         dashboardTelemetry.addData("lift ticks", intake.getLiftTicks());
-        dashboardTelemetry.addData("left drive ticks", drive.getLeftTicks());
-        dashboardTelemetry.addData("right drive ticks", drive.getRightTicks());
         dashboardTelemetry.addData("Status", "Run Time: " + runtime.toString());
         dashboardTelemetry.update();
     }
 
     @Override
     public void stop() {
-        drive.stop();
         intake.stop();
         shooter.stop();
     }
