10f52277fda684f888b1b2fb25d8370357a1a105
amcl7032 <vaz.nightwalker@gmail.com>
2026-03-20 13:01:24 +0100

    if (g1.wasJustPressed(GamepadKeys.Button.B)){             myShooter.toggleMotor();
            }else{
    
            }
    
            if (g1.wasJustPressed(GamepadKeys.Button.X)){
                myShooter.servopos2();
            }
            else if (g1.wasJustReleased(GamepadKeys.Button.X)) {
                myShooter.servopos1();
            }

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/atou.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/atou.java
@@ -2,23 +2,28 @@ package org.firstinspires.ftc.teamcode.Teleops;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
 import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
-@Autonomous(name = "atuo")
+@Autonomous(name = "auto")
 public class atou extends LinearOpMode{
+
+
     public void runOpMode(){
         waitForStart();
         int COUNT = 3;
         DriveSubsystem myDriveTrain = new DriveSubsystem(hardwareMap);
-        Shooter SUPA_GUN = new Shooter(hardwareMap);
-        myDriveTrain.drive2(1,0,0);
+        myDriveTrain.setHeadingToMaintain(myDriveTrain.getCurrentHeadingDeg());
         sleep(1000);
-        SUPA_GUN.toggleMotor();
-        //this is the motor and servo code to launch the balls autonomously
-        SUPA_GUN.toggleMotor();
+        Shooter SUPA_GUN = new Shooter(hardwareMap);
+        myDriveTrain.drive2(-0.25,-0.5,0);
+        sleep(1000);
+        for (int i = 1; i < 10; i++){
+            SUPA_GUN.toggleMotor();
+        }
         for (int i = 0; i < COUNT; i++) {
+            for (int v = 1; v < 10; v++){
+                SUPA_GUN.toggleMotor();
+            }
             sleep(1000);
             SUPA_GUN.servopos2();
             sleep(500);
